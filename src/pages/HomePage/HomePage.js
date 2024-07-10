55b26e2bd3dc50b6db20c1ade31fd6a0df130b5f
HOANG Nghia Hieu <NghiaHieu.HOANG@slib.com>
2024-07-10 14:19:16 +0200

    prettier home section

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import { FaGithub, FaEnvelope, FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 import './HomePage.css';
 
 const HomePage = () => {
+    const codeString = `// Define Hieu
+const engineer = {
+    name: 'Hieu',
+    expertise: 'AI/Machine Learning',
+    technicalSkills: {
+        programming: ['Python', 'JavaScript', 'Java', 'Go', 'C', 'SQL'],
+        machineLearning: ['TensorFlow', 'PyTorch'],
+        webDevelopment: ['React', 'Node.js', 'Express', 'Angular', 'Spring', 
+                         'MongoDB', 'PostgreSQL'],
+        devOps: ['Docker', 'Kubernetes', 'Jenkins', 'Ansible', 'Git'],
+    },
+    softSkills: ['Problem Solving', 'Teamwork', 'Communication', 'Leadership'],
+}`
     return (
         <>
             <Header />
@@ -14,12 +29,13 @@ const HomePage = () => {
                     <h1 className="home-title">Hoang Nghia Hieu</h1>
                     <h2 className="home-subtitle">I teach machine to do things</h2>
                     <p className="home-description">
-                    I am an engineering student at the National Institute of Applied Sciences of Lyon (INSA Lyon) and an AI engineer apprentice at SLIB. 
-                    With hands-on experience in the FinTech industry, I am passionate about AI and Machine Learning, particularly in designing and 
-                    implementing AI solutions for finance.
+                        I am an engineering student at <a href="https://www.insa-lyon.fr/" target="_blank" rel="noreferrer noopener" className="highlight-link">National Institute of Applied Sciences of Lyon (INSA Lyon)</a> and
+                        an AI engineer apprentice at <a href="https://www.slib.com/" target="_blank" rel="noreferrer noopener" className="highlight-link">SLIB</a>.
+                        With hands-on experience in the FinTech industry, I am passionate about AI and Machine Learning, particularly in designing and
+                        implementing AI solutions in the financial sector.
                     </p>
                     <div className="home-social-links">
-                        <a href="" target='_blank' rel='noreferrer noopener' className="home-social-link">
+                        <a href="https://github.com/nhh1603" target='_blank' rel='noreferrer noopener' className="home-social-link">
                             <FaGithub />
                         </a>
                         <a href="" target='_blank' rel='noreferrer noopener' className="home-social-link">
@@ -43,23 +59,9 @@ const HomePage = () => {
                         <div className="circle green"></div>
                     </div>
                     <pre>
-                        <code>
-                            {`// Example Code
-const engineer = {
-    name: 'Hieu',
-    expertise: 'AI/Machine Learning',
-    technicalSkills: {
-        programming: ['Python', 'JavaScript', 'Java', 'Go', 'C', 'SQL'],
-        machineLearning: ['TensorFlow', 'PyTorch'],
-        webDevelopment: ['React', 'Node.js', 'Express', 'Angular', 'Spring', 
-                         'MongoDB', 'PostgreSQL'],
-        devOps: ['Docker', 'Kubernetes', 'Jenkins', 'Ansible', 'Git'],
-    },
-    softSkills: ['Problem Solving', 'Teamwork', 'Communication', 'Leadership'],
-}
-
-greet();`}
-                        </code>
+                        <SyntaxHighlighter language="javascript" style={vscDarkPlus} wrapLines={true} wrapLongLines={true} customStyle={{ fontSize: '14px' }}>
+                            {codeString}
+                        </SyntaxHighlighter>
                     </pre>
                 </div>
             </section>
